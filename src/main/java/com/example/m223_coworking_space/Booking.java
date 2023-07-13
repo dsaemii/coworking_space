45316f0550246f7095633cfef6847e11d1dfc888
Samira Stragiotti <samira.stragiotti@stud.kbw.ch>
2023-07-13 10:33:42 +0200

    Booking Entity added

---
+++ src/main/java/com/example/m223_coworking_space/Booking.java
@@ -0,0 +1,29 @@
+package com.example.m223_coworking_space;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.sql.Date;
+
+@Entity
+public class Booking {
+    private @Id
+    @GeneratedValue Integer booking_id;
+
+    private Integer user_idfs;
+    private Date date;
+    private boolean morning;
+    private boolean afternoon;
+    private boolean state;
+    private String comment;
+
+    public Booking() {}
+
+    public Booking(Integer user_idfs, Date date, boolean morning, boolean afternoon) {
+        this.user_idfs = user_idfs;
+        this.date = date;
+        this.morning = morning;
+        this.afternoon = afternoon;
+    }
+}

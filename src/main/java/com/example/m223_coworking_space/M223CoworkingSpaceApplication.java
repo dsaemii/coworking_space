957e90db8c2cbc3de9b62b5e4462a03807d26599
Samira Stragiotti <samira.stragiotti@stud.kbw.ch>
2023-07-13 10:32:44 +0200

    Set up

---
+++ src/main/java/com/example/m223_coworking_space/M223CoworkingSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.m223_coworking_space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M223CoworkingSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M223CoworkingSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/m223_coworking_space/M223CoworkingSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m223_coworking_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M223CoworkingSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

1ee676c1a02bc53f989093c6ed3a8840778d42cb
Samira Stragiotti <samira.stragiotti@stud.kbw.ch>
2023-07-14 09:38:17 +0200

    changed name (to App)

--- src/main/java/com/example/m223_coworking_space/domain/App.java
+++ src/main/java/com/example/m223_coworking_space/domain/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class M223CoworkingSpaceApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(M223CoworkingSpaceApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }

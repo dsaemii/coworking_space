3bb0773cd710ecd69881a43d619a37430f1fde31
Samira Stragiotti <samira.stragiotti@stud.kbw.ch>
2023-07-14 13:17:42 +0200

    add getter and setter

--- src/main/java/com/example/m223_coworking_space/domain/Booking.java
+++ src/main/java/com/example/m223_coworking_space/domain/Booking.java
@@ -25,4 +25,60 @@ public class Booking {
         this.morning = morning;
         this.afternoon = afternoon;
     }
+
+    public Integer getBooking_id() {
+        return booking_id;
+    }
+
+    public void setBooking_id(Integer booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    public User getUser_idfs() {
+        return user_idfs;
+    }
+
+    public void setUser_idfs(User user_idfs) {
+        this.user_idfs = user_idfs;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isMorning() {
+        return morning;
+    }
+
+    public void setMorning(boolean morning) {
+        this.morning = morning;
+    }
+
+    public boolean isAfternoon() {
+        return afternoon;
+    }
+
+    public void setAfternoon(boolean afternoon) {
+        this.afternoon = afternoon;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
